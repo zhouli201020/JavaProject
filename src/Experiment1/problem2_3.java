843b4758d426409273e314b3c31a3065633b15c3
zhouli201020 <zhouli201020@gmail.com>
2022-10-10 14:18:10 +0800

    优化了最后一题（输出图案）中，内层循环条件的形式

--- src/Experiment1/problem2_3.java
+++ src/Experiment1/problem2_3.java
@@ -14,7 +14,7 @@ public class problem2_3 {
             for (int j = 1; j <= i - 1; j++) {
                 System.out.print(" ");
             }
-            for (int k = 1; k <= 2 * (n + 1 - i) - 1; k++) {
+            for (int k = 1; k <= 2 * n - (2 * i - 1); k++) {
                 System.out.print("*");
             }
             System.out.println();
