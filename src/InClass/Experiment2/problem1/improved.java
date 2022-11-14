14b5466bc9fe654d5b0caf3a5196a5a14eaa045c
zhouli201020 <zhouli201020@gmail.com>
2022-11-14 16:19:01 +0800

    问题一的提高要求

---
+++ src/InClass/Experiment2/problem1/improved.java
@@ -0,0 +1,30 @@
+/**
+ * 问题一的提高要求
+ */
+package InClass.Experiment2.problem1;
+
+public class improved {
+    public static void main(String[] args) {
+        int[] array = {16, 21, 10, 62, 34};
+        System.out.println("操作前的数组为：");
+        basic.outputArray(array);
+        System.out.println();
+        evenArray(array);//调用方法将数组元素变为偶数
+        System.out.println("操作后的数组为：");
+        basic.outputArray(array);
+    }
+
+    /**
+     * 将数组元素变为偶数
+     *
+     * @param array 所操作的数组
+     */
+    static void evenArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                array[i] *= 2;
+            }
+        }
+    }
+
+}
