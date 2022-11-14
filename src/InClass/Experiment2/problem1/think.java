d8d456ae5ff473dc461ab2089166d64520f69f37
zhouli201020 <zhouli201020@gmail.com>
2022-11-14 16:37:42 +0800

    问题一的思考

---
+++ src/InClass/Experiment2/problem1/think.java
@@ -0,0 +1,26 @@
+/**
+ * 问题一的思考
+ */
+package InClass.Experiment2.problem1;
+
+public class think {
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(n);
+        evenint(n);
+        System.out.println(n);
+    }
+
+    static void evenint(int num) {
+        if (num % 2 != 0) {
+            num *= 2;
+        }
+    }
+}
+/**
+ * 说明：
+ * 1.对于基本数据类型参数，在调用方法时传递的是实参的值，方法中的操作并不会改变实参的值
+ * 2.而对于数组类型参数，参数值是数组的引用，给方法传递的是这个引用。从语义上来讲，
+ * 最好的描述就是参教传递的是共享信息（pass-by-sharing)，即方法中的数组和传递的数
+ * 组是一样的。所以，如果改变方法中的数组，将会看到方法外的数组也变化了。
+ */
