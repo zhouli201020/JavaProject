07007beba91b4040846d20169d73264d9848a9bd
zhouli201020 <zhouli201020@gmail.com>
2022-11-14 16:58:13 +0800

    问题二的基本要求

---
+++ src/InClass/Experiment2/problem2/basic.java
@@ -0,0 +1,41 @@
+/**
+ * 问题二的基本要求
+ */
+package InClass.Experiment2.problem2;
+
+import java.util.Scanner;
+
+public class basic {
+    public static void main(String[] args) {
+        System.out.println("请输入行数：");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for (int i = 0; i < n; i++) {
+            space(n, i);
+            eightstar();
+            System.out.println();
+        }
+    }
+
+    /**
+     * 输出空格
+     *
+     * @param n     总行数
+     * @param index 当前行数（从0开始）
+     */
+    static void space(int n, int index) {
+        for (int i = n - 1 - index; i > 0; i--) {
+            System.out.print(" ");
+        }
+    }
+
+    /**
+     * 打印8个*
+     */
+    static void eightstar() {
+        for (int i = 0; i < 8; i++) {
+            System.out.print("*");
+        }
+    }
+
+}
