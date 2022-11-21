fda6e38b36c4496db6b0007214365ec2091610f0
zhouli201020 <zhouli201020@gmail.com>
2022-11-21 20:19:02 +0800

    Random类示例

--- src/IntroductionToJavaProgramming/test.java
+++ src/IntroductionToJavaProgramming/test.java
@@ -1,4 +1,19 @@
 package IntroductionToJavaProgramming;
 
+import java.util.Date;
+import java.util.Random;
+
 public class test {
+    public static void main(String[] args) {
+        Random rand1 = new Random(3);
+        System.out.println("From rand1:");
+        for(int i=0;i<10;i++){
+            System.out.print(rand1.nextInt(1000)+" ");
+        }
+        Random rand2 = new Random(3);
+        System.out.println("\nFrom rand2:");
+        for(int i=0;i<10;i++){
+            System.out.print(rand2.nextInt(1000)+" ");
+        }
+    }
 }
