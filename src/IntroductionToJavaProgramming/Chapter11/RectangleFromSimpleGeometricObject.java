176825757916110215da85ec067d23cb8afad846
zhouli201020 <zhouli201020@gmail.com>
2022-12-14 13:38:10 +0800

    程序清单11-3

---
+++ src/IntroductionToJavaProgramming/Chapter11/RectangleFromSimpleGeometricObject.java
@@ -0,0 +1,65 @@
+package IntroductionToJavaProgramming.Chapter11;
+
+import InClass.Experiment3.RentedVehicle;
+
+public class RectangleFromSimpleGeometricObject extends SimpleGeometricObject {
+    private double width;
+    private double height;
+
+    public RectangleFromSimpleGeometricObject() {
+
+    }
+
+    public RectangleFromSimpleGeometricObject(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public RectangleFromSimpleGeometricObject(double width, double height, String color, boolean filled) {
+        super(color, filled);
+        this.width = width;
+        this.height = height;
+    }
+
+    /**
+     * Return width
+     */
+    public double getWidth() {
+        return width;
+    }
+
+    /**
+     * Set a new width
+     */
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    /**
+     * Return height
+     */
+    public double getHeight() {
+        return height;
+    }
+
+    /**
+     * Set a new height
+     */
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    /**
+     * Return area
+     */
+    public double getArea() {
+        return width * height;
+    }
+
+    /**
+     * Return perimeter
+     */
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+}
