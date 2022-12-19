deec18d68172dd8d635bbaf3f96dfdde179eef30
zhouli201020 <zhouli201020@gmail.com>
2022-12-19 17:10:37 +0800

    test

--- src/test.java
+++ src/test.java
@@ -1,8 +1,2 @@
-public class test{
-    public static void main(String[] args) {
-        System.out.println(sum(1,2));
-    }
-    static double sum(int a, double b){
-        return a+b;
-    }
+public class test {
 }
